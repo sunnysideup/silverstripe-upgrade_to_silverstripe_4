5c51a3b638c7d68c25ecfadd585d406a009fc89f
sunnysideup <sunnysideupdevs@gmail.com>
2018-05-07 05:39:16 +1200

    PATCH: module clean-up

---
+++ tests/UpgradeToSilverstripe4Test.php
@@ -0,0 +1,15 @@
+<?php
+
+class UpgradeToSilverstripe4Test extends SapphireTest
+{
+    protected $usesDatabase = false;
+
+    protected $requiredExtensions = array();
+
+    public function TestDevBuild()
+    {
+        $exitStatus = shell_exec('php framework/cli-script.php dev/build flush=all  > dev/null; echo $?');
+        $exitStatus = intval(trim($exitStatus));
+        $this->assertEquals(0, $exitStatus);
+    }
+}

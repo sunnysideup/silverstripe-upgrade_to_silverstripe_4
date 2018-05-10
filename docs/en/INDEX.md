84b24e3f8c4e450d5fba11445fbda020d953fde8
Nicolaas Thiemen Francken @ B&C <github@sunnysideup.co.nz>
2018-05-10 17:27:06 +1200

    MAJOR: rework

--- docs/en/INDEX.md
+++ docs/en/INDEX.md
@@ -1,78 +1,79 @@
-# Silverstripe upgrade to silverstripe 4 module
-[![Build Status](https://travis-ci.org/sunnysideup/silverstripe-upgrade_to_silverstripe_4.svg?branch=master)](https://travis-ci.org/sunnysideup/silverstripe-upgrade_to_silverstripe_4)
-[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/sunnysideup/silverstripe-upgrade_to_silverstripe_4/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/sunnysideup/silverstripe-upgrade_to_silverstripe_4/?branch=master)
-[![codecov.io](https://codecov.io/github/sunnysideup/silverstripe-upgrade_to_silverstripe_4/coverage.svg?branch=master)](https://codecov.io/github/sunnysideup/silverstripe-upgrade_to_silverstripe_4?branch=master)
-
-[![Latest Stable Version](https://poser.pugx.org/sunnysideup/upgrade_to_silverstripe_4/version)](https://packagist.org/packages/sunnysideup/upgrade_to_silverstripe_4)
-[![License](https://poser.pugx.org/sunnysideup/upgrade_to_silverstripe_4/license)](https://packagist.org/packages/sunnysideup/upgrade_to_silverstripe_4)
-[![Monthly Downloads](https://poser.pugx.org/sunnysideup/upgrade_to_silverstripe_4/d/monthly)](https://packagist.org/packages/sunnysideup/upgrade_to_silverstripe_4)
+This module aims to help developers upgrade SS3 modules to SS4 without doing any manual rewriting. You can provide a list of modules and run the code below.  This will create a branch in your module that is SS4 ready. After inspection you can then merge this into `dev/master` as you see fit.
 
 
-## Documentation
+# prerequisites before you start
 
+ - create 3.6 branch so that you can keep adding PATCHes to your SS3 version.
+ - create a tag (OPTIONAL)
+ - module needs to be listed on packagist
+ - composer file needs to follow this pattern (installer-name requirement may be dropped in the future)
 
-
- * [Developer Docs](docs/en/INDEX.md)
- * [User Guide](docs/en/userguide.md)
- * [API Docs](http://docs.ssmods.com/sunnysideup/upgrade_to_silverstripe_4/classes.xhtml)
-
-
-## Requirements
-
-
-
-See [composer.json](composer.json) for details
-
-
-### Suggested Modules
-
-
-
-See [composer.json](composer.json) for details
-
-
-## Installation
-
-
-```
-composer require sunnysideup/upgrade_to_silverstripe_4
+```json
+{
+    "name": "sunnysideup/google-calendar-interface",
+    "type": "silverstripe-module",
+    ...
+    ...
+    ...
+    "extra": {
+        "installer-name": "google-calendar-interface"
+    },
+}
 ```
 
-### Configuration
+# usage:
+1. Run in your web root (also see below):
+`git clone git@github.com:sunnysideup/silverstripe-upgrade_to_silverstripe_4.git`
+
+2. Create a new php file (e.g. `index.php`) in your root dir (or anywhere else you can run it):
+
+```php
+<?php
+require_once('silverstripe-upgrade_to_silverstripe_4/src/MetaUpgrader.php');
+$obj = MetaUpgrader::create()
+    ->setRootDir('/var/www')
+    ->setUpgradeDirName('upgradeto4')
+    ->setVendorName('sunnysideup')
+    ->setArrayOfModules(
+      [
+          'share_this_simple'
+      ]
+    )
+    ->setNameOfTempBranch('4.1-TEMP-upgrade')
+    ->setRunImmediately(false)
+    ->setComposerEnvironmentVars('COMPOSER_HOME="/home/UserName"')
+    ->setLocationOfUpgradeModule('~/.composer/vendor/bin/upgrade-code')
+    ->run();
+```
+run the file to upgrade your modules.
+
+3. inspect your new branch that is SS4 ready and merge it into `dev-master` as you see fit.
 
 
+# options:
 
-In the `_config` folder you will find the `upgrade_to_silverstripe_4.yml.example`
-file that shows options for the configuration of this module.
+### temp branch
 
-We recommend that you:
+`->setNameOfTempBranch('4.1-TEMP-upgrade')` : a temporary branch will be added to your module.  
+All upgrade changes will be committed to this branch. **Careful!  This branch will be deleted every time you run the update process so that you can run the update process many times.**
 
-  1. copy these `upgrade_to_silverstripe_4.yml.example` files into your
-`mysite/_config` folder (where available - otherwise search for `private static $` in the module to see what can be configured)
-  2. remove the .example extension,
-  3. delete the lines you do not care about, and
-  4. adjust the configurations that you would like to use.
+### vendor name
 
+`->setVendorName('SunnySideUp')` : you can only upgrade modules for one vendor at the time.
 
-## Contributing
+### root directory
 
+`->setRootDir('/var/www/')`  This is meant to be the directory where you do the work, where you usually save your websites that you work on locally.
 
+### upgrade directory
 
-We welcome any contributions. See [CONTRIBUTING.md](CONTRIBUTING.md) for more details.
+`->setUpgradeDirName('upgradeto4')`. This is the name of the directory that is created in the root dir where the upgrade takes place. **Careful! Only use this directory for automated work as it will be deleted when you run the upgrade again.**
 
-## Paid assistance
+### list of modules
 
+`->setArrayOfModules([])`. This is the name as listed on packagist e.g. `sunnysideup/metatags` should be listed as `metatags`.
 
+### run immediately or create bash script?
 
-You can pay us to create an improved / adapted version of this module for your own projects.  Please contact us if you like to find out more: [www.sunnysideup.co.nz](http://www.sunnysideup.co.nz).  For exmaple, we can write tests for this module.  
-
-## Author
-
-
-
-Sunny Side Up Ltd.
-
-
-## Care to see more modules?
-
-To find other modules, please visit [ssmods.com](http://ssmods.com/).
+`->setRunImmediately(false)`. By default, the script will not do any actual upgrading, but rather, it will output a bash script that does the actual upgrading.
+When you set `runImmediately` to true, the PHP code will use the `exec` function to output commands on the `command line` immediately. **Careful! Even if you do not run the code immediately, a bunch of code will still be executed on the command line to inspect the module.**
--- src/MetaUpgrader.php
+++ src/MetaUpgrader.php
@@ -3,7 +3,7 @@
 
 class MetaUpgrader {
 
-    private static $_singleton
+    private static $_singleton = null;
 
     public static function create()
     {
@@ -11,7 +11,7 @@ class MetaUpgrader {
             self::$_singleton = new MetaUpgrader();
         }
 
-        return self::$_singleton();
+        return self::$_singleton;
     }
 
     protected $nameOfTempBranch = 'temp-upgradeto4-branch';
@@ -70,110 +70,376 @@ class MetaUpgrader {
         return $this;
     }
 
+    //e.g. COMPOSER_HOME="/home/UserName"
+    protected $composerEnvironmentVars = '';
+
+    public function setComposerEnvironmentVars($s)
+    {
+        $this->composerEnvironmentVars = $s;
+
+        return $this;
+    }
+
+    //e.g. '~/.composer/vendor/bin/upgrade-code'
+    protected $locationOfUpgradeModule = 'upgrade-code';
+
+    public function setLocationOfUpgradeModule($s)
+    {
+        $this->locationOfUpgradeModule = $s;
+
+        return $this;
+    }
+
+    protected $upgradeDir = '';
+
+    protected $moduleName = '';
+
+    protected $moduleFolder = '';
+
+    protected $vendorNameSpace = '';
+
+    protected $moduleNameSpace = '';
+
     function run()
     {
+        $this->startOutput();
         $this->execMe(
-            true,
-            'echo "===================== START ======================"'
-            ' ###########################'
+            false,
+            'echo "===================== START ======================"',
+            ' show that the task is starting'
         );
-        $upgradeDir = $this->rootDir.'/'.$this->upgradeDirName;
+        $this->upgradeDir = $this->rootDir.'/'.$this->upgradeDirName;
+        $this->vendorNameSpace = $this->camelCase($this->vendorName);
+        if(! $this->vendorNameSpace) {
+            user_error('ERROR IN VENDOR NAME SPACE', E_USER_ERROR);
+            die('------------');
+        }
         foreach($this->arrayOfModules as $moduleFolderName) {
-            $moduleName = $this->camelCase($moduleFolderName);
-            $moduleFolder = $upgradeDir . '/' . $moduleFolderName;
+            $this->moduleName = $moduleFolderName;
+            $this->moduleFolder = $this->upgradeDir . '/' . $moduleFolderName;
+            $this->moduleNameSpace = $this->camelCase($moduleFolderName);
+            if(! $this->moduleNameSpace) {
+                user_error('ERROR IN VENDOR NAME SPACE', E_USER_ERROR);
+                die('------------');
+            }
             $this->execMe(
-                true,
-                'cd '.$this->rootDir,
-                'change to root dir: '.$this->rootDir
+                false,
+                'echo "______ '.$this->moduleName.' in '.$this->moduleFolder . '_____ "',
+                'starting new module: '.$this->moduleName.' in '.$this->moduleFolder
             );
+
+            $this->runResetUpgradeDir();
+
+            $this->upgradeDir = $this->checkIfPathExistsAndCleanItUp($this->upgradeDir);
             $this->execMe(
-                true,
-                'rm '.$upgradeDir. ' -rf',
-                'remove the temp upgrade dir: '.$upgradeDir
+                false,
+                'cd '.$this->upgradeDir,
+                'move into the upgrade directory: '.$this->upgradeDir.' to start working in it'
             );
+
             $this->execMe(
-                true,
-                'composer create-project silverstripe/installer '.$upgradeDir.' ^4'
-                'set up vanilla install of 4.0+ in: '.$upgradeDir
+                false,
+                'composer require '.$this->vendorName.'/'.$this->moduleName.':dev-master',
+                'checkout dev-master of '.$this->vendorName.'/'.$this->moduleName
             );
+
+            $this->moduleFolder = $this->checkIfPathExistsAndCleanItUp($this->moduleFolder);
             $this->execMe(
-                true,
-                'cd '.$upgradeDir,
-                'change dir to temp upgrade dir: '.$upgradeDir
+                false,
+                'cd '.$this->moduleFolder,
+                'change to dir of actual module: '.$this->moduleFolder
             );
+
             $this->execMe(
-                true,
-                'composer require '.$this->vendorName.'/'.$moduleName.':dev-master',
-                'checkout dev master '
-            );
-            $this->execMe(
-                true,
-                'cd '.$moduleFolder,
-                'change dir to module folder: '.$moduleFolder
-            );
-            $this->execMe(
-                true,
+                false,
                 'git branch -d '.$this->nameOfTempBranch,
                 'delete upgrade branch locally: '.$this->nameOfTempBranch
             );
+
             $this->execMe(
-                true,
+                false,
                 'git push origin --delete '.$this->nameOfTempBranch,
                 'delete upgrade branch remotely: '.$this->nameOfTempBranch
             );
-            if(file_exists($moduleFolder . '/code')) {
-                $codeDir = $moduleFolder . '/code'
-            } elseif(file_exists($codeDir = $moduleFolder . '/src')) {
-                $codeDir = $moduleFolder . '/src';
-            } else {
-                user_error('Can not find code dir for '.$moduleFolder, E_USER_NOTICE);
-                continue;
-            }
-            $this->execMe(
-                true,
-                'git checkout -b '.$this->nameOfTempBranch,
-                '(re)create the upgrade branch ...'
-            );
-            $directories = glob($codeDir , GLOB_ONLYDIR);
-            foreach($directories as $dir) {
-                $nameSpaceAppendix = str_replace($codeDir, '', $dir);
-                $nameSpaceAppendix = str_replace('/', '\\', $nameSpaceAppendix);
 
-                $nameSpace = $this->vendorName.'\\'.$moduleName.'\\'.$nameSpaceAppendix;
-                $nameSpaceArray = explode('\\', $nameSpace);
-                $nameSpaceArrayNew = [];
-                foreach($nameSpaceArray as $nameSpaceSnippet) {
-                    if($nameSpaceSnippet) {
-                        $nameSpaceArrayNew[] = $this->camelCase($nameSpaceSnippet);
+            $this->execMe(
+                false,
+                'git checkout -b '.$this->nameOfTempBranch,
+                'create and checkout new branch: '.$this->nameOfTempBranch
+            );
+
+            $this->runUpdateComposerRequirements($this->moduleFolder, 'silverstripe/framework', '~4.0');
+
+            $this->runUpdateComposerRequirements($this->moduleFolder, 'silverstripe/cms', '~4.0');
+
+            $this->runCommitAndPush('MAJOR: upgrading composer requirements to SS4');
+
+            $this->runResetUpgradeDir();
+
+            $this->execMe(
+                false,
+                $this->composerEnvironmentVars.' composer create-project silverstripe/installer '.$this->upgradeDir.' ^4',
+                'set up vanilla install of 4.0+ in: '.$this->upgradeDir
+            );
+
+
+            $this->upgradeDir = $this->checkIfPathExistsAndCleanItUp($this->upgradeDir);
+            $this->execMe(
+                false,
+                'cd '.$this->upgradeDir,
+                'move into the upgrade directory: '.$this->upgradeDir.' to start working in it'
+            );
+
+
+            $this->execMe(
+                false,
+                'composer require '.$this->vendorName.'/'.$this->moduleName.':dev-'.$this->nameOfTempBranch.' ', //--prefer-source --keep-vcs
+                'add '.$this->vendorName.'/'.$this->moduleName.':dev-'.$this->nameOfTempBranch.' to install'
+            );
+
+            $this->moduleFolder = $this->checkIfPathExistsAndCleanItUp($this->moduleFolder);
+            $this->execMe(
+                false,
+                'rm '.$this->moduleFolder.' -rf',
+                'we will remove the item again: '.$this->moduleFolder.' so that we can reinstall with vcs data.'
+            );
+
+            $this->execMe(
+                false,
+                'composer update',
+                'lets retrieve the module again to make sure we have the vcs data with it!'
+            );
+
+            if($this->runImmediately) {
+                if(file_exists($this->moduleFolder . '/code')) {
+                    $codeDir = $this->moduleFolder . '/code';
+                } elseif(file_exists($codeDir = $this->moduleFolder . '/src')) {
+                    $codeDir = $this->moduleFolder . '/src';
+                } else {
+                    user_error('Can not find code dir for '.$this->moduleFolder, E_USER_NOTICE);
+                    continue;
+                }
+
+                $directories = glob($codeDir , GLOB_ONLYDIR);
+                foreach($directories as $dir) {
+                    $nameSpaceAppendix = str_replace($codeDir, '', $dir);
+                    $nameSpaceAppendix = str_replace('/', '\\', $nameSpaceAppendix);
+
+                    $nameSpace = $this->vendorNameSpace.'\\'.$this->moduleNameSpace.'\\'.$nameSpaceAppendix;
+                    $nameSpaceArray = explode('\\', $nameSpace);
+                    $nameSpaceArrayNew = [];
+                    foreach($nameSpaceArray as $nameSpaceSnippet) {
+                        if($nameSpaceSnippet) {
+                            $nameSpaceArrayNew[] = $this->camelCase($nameSpaceSnippet);
+                        }
+                    }
+                    $nameSpace = implode('\\', $nameSpaceArrayNew);
+                    foreach($this->scanDirectory($dir, '.php') as $file) {
+                        $this->execMe(
+                            false,
+                            'php '.$this->locationOfUpgradeModule.' add-namespace "'.$nameSpace.'" ./'.$dir.'/.'.$file.'  --write -vvv',
+                            'adding name space: '.$nameSpace.' to ./'.$dir.'/.'.$file
+                        );
                     }
                 }
-                $nameSpace = implode('\\', $nameSpaceArrayNew)
-                foreach($this->scanDirectory($dir, '.php') as $file) {
-                    $this->execMe(
-                        true,
-                        'php ~/.composer/vendor/bin/upgrade-code add-namespace "'.$nameSpace.'" ./'.$dir.'/.'.$file.'  --write -vvv',
-                        'adding name spaces'
-                    );
-                }
+            } else {
+                //@todo: we assume 'code' for now ...
+                $codeDir = $this->moduleFolder . '/code';
             }
+
+
         }
-        $this->execMe('echo "===================== END ======================"');
+        $this->execMe(
+            false,
+            'echo "===================== END ======================"',
+            'show that we are finished'
+        );
+        $this->endOutput();
+    }
+
+    protected function runResetUpgradeDir()
+    {
+        $this->rootDir = $this->checkIfPathExistsAndCleanItUp($this->rootDir);
+        $this->execMe(
+            false,
+            'cd '.$this->rootDir,
+            'change back to the root directory: '.$this->rootDir
+        );
+
+        $this->execMe(
+            false,
+            'rm '.$this->upgradeDir. ' -rf',
+            'remove the upgrade dir: '.$this->upgradeDir
+        );
+
+        $this->execMe(
+            false,
+            'mkdir '.$this->upgradeDir. '',
+            'create upgrade directory: '.$this->upgradeDir
+        );
+
+    }
+
+    protected function runCommitAndPush($message)
+    {
+        $this->execMe(
+            false,
+            'git add . -A',
+            'git add all'
+        );
+
+        $this->execMe(
+            false,
+            'git commit . -m "'.$message.'"',
+            'commit changes in composer.json'
+        );
+
+        $this->execMe(
+            false,
+            'git push origin '.$this->nameOfTempBranch,
+            'pushing changes to server on the '.$this->nameOfTempBranch.' branch'
+        );
+    }
+
+
+    protected function runUpdateComposerRequirements($folder, $module, $newVersion)
+    {
+        $location = $folder.'/composer.json';
+        $this->execMe(
+            false,
+
+            'php -r  \''
+            .'$jsonString = file_get_contents("'.$location.'"); '
+            .'$data = json_decode($jsonString, true); '
+            .'if(isset($data["require"]["'.$module.'"])) { '
+            .'    $data["require"]["'.$module.'"] = "'.$newVersion.'"; '
+            .'}'
+            .'$newJsonString = json_encode($data, JSON_PRETTY_PRINT); '
+            .'file_put_contents("'.$location.'", $newJsonString); '
+            .'\'',
+
+            'replace in '.$location.' the require for '.$module.' with '.$newVersion
+        );
     }
 
     protected function execMe($alwaysRun, $line, $comment)
     {
         echo $this->newLine();
         echo $this->newLine();
-        echo '# '.$comment;
+        if ($this->isHTML()) {
+            echo '<strong># '.$comment .'</strong><br />';
+            if($this->runImmediately || $alwaysRun) {
+                //do nothing
+            } else {
+                echo '<div style="color: transparent">tput setaf 33; echo " _____ : '.addslashes($comment) .'" ____ </div>';
+            }
+        } else {
+            echo '# '.$comment;
+            echo $this->newLine();
+        }
         echo $line;
         if($this->runImmediately || $alwaysRun) {
-            exec($line);
+            $outcome = exec($line.'  2>&1 ', $error, $return);
+            if($return) {
+                print_r($error);
+                $this->endOutput();
+                die('------ STOPPED -----');
+            } else {
+                echo ' <i>[DONE]</i>';
+            }
+        }
+        if ($this->isHTML()) {
+            ob_flush();
+            flush();
+        }
+    }
+
+    protected function isCommandLine() : bool
+    {
+        if (php_sapi_name() == "cli") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    protected function isHTML() : bool
+    {
+        return $this->isCommandLine() ? false : true;
+    }
+
+    protected function startOutput()
+    {
+        if ($this->isHTML()) {
+            // Turn off output buffering
+            // ini_set('output_buffering', 'off');
+            // // Turn off PHP output compression
+            // ini_set('zlib.output_compression', false);
+            //
+            // //Flush (send) the output buffer and turn off output buffering
+            // //ob_end_flush();
+            // while (@ob_end_flush());
+            //
+            // // Implicitly flush the buffer(s)
+            // ini_set('implicit_flush', true);
+            // ob_implicit_flush(true);
+            //
+            // //prevent apache from buffering it for deflate/gzip
+            // header("Content-type: text/plain");
+            // header('Cache-Control: no-cache'); // recommended to prevent caching of event data.
+
+            echo '
+            <!DOCTYPE html>
+            <html lang="en-US">
+            <head>
+            <meta charset="UTF-8">
+            <title>Title of the document</title>
+            </head>
+
+            <body>
+                <pre><code class="sh">#!/bin/bash<br />';
+            ob_flush();
+            flush();
+
+        }
+    }
+
+
+
+    protected function endOutput()
+    {
+        if ($this->isHTML()) {
+            $dir = dirname(dirname(__FILE__));
+            // $css = file_get_contents($dir.'/javascript/styles/default.css');
+            // $js = file_get_contents($dir.'/javascript/highlight.pack.js');
+            // echo '</code></pre>
+            // <script>
+            //     '.$js.'
+            //     hljs.initHighlightingOnLoad();
+            // </script>
+            echo '
+            <style>
+                html, body {padding: 0; margin: 0; min-height: 100%; height: 100%; background-color: #300a24;color: #fff;}
+                pre {
+                    font-family: Consolas,Monaco,Lucida Console,Liberation Mono,DejaVu Sans Mono,Bitstream Vera Sans Mono,Courier New, monospace;
+                }
+                strong {display: block; color: teal;}
+                i {color: green; font-style: normal;}
+                .hljs-string {color: yellow;}
+                .hljs-built_in {color: #ccc;}
+            </style>
+            </body>
+            </html>
+
+            ';
+            ob_flush();
+            flush();
         }
     }
 
     protected function newLine()
     {
-        if (php_sapi_name() == "cli") {
+        if ($this->isCommandLine()) {
             return PHP_EOL;
         } else {
             return '<br />';
@@ -200,7 +466,7 @@ class MetaUpgrader {
         foreach($dirContent as $key => $content) {
             $path = $rootDir.'/'.$content;
             $ext = substr($content, strrpos($content, '.') + 1);
-            if(is_readable($path) {
+            if(is_readable($path)) {
                 if(is_file($path)) {
                     if(in_array($ext, $allowext)) {
                         $list[] = $path;
@@ -213,5 +479,20 @@ class MetaUpgrader {
         }
         return $list;
     }
+
+    protected function checkIfPathExistsAndCleanItUp($path)
+    {
+        if($this->runImmediately) {
+            $path = realpath($path);
+            if(! file_exists($path)) {
+                die('ERROR! Could not find: '.$path);
+            }
+        } else {
+            $path = str_replace('//', '/', $path);
+        }
+        return $path;
+    }
+
+
 
 }

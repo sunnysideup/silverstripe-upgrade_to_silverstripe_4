62006e56c7cc008bc1d1bcb26e99807ab8305673
LABCAT <shane.watters.nz@gmail.com>
2019-12-03 11:17:43 +1300

    Improvements to Ss31ToSs37 recipe.

--- src/UpgradeRecipes/Ss31ToSs37.php
+++ src/UpgradeRecipes/Ss31ToSs37.php
@@ -23,69 +23,6 @@ class Ss33ToSs37 extends BaseClass
     # TASKS
     #########################################
 
-    /**
-     * An array of all the 'taskName's of the tasks that you wish to run during the execution of this upgrader task.
-     * This array can be overriden in the example-index.php file that you create.
-     * You can enter a full name space if you need to.
-     * The final -x will be removed.  We add -1 or -2 to run the same task multiple times.
-     *
-     * @var array
-     */
-    protected $listOfTasks = [
-        //Step1: Prepare
-        'CheckThatFoldersAreReady' => [],
-        'ResetWebRootDir-1' => [],
-
-        'CheckoutDevMaster-1' => [],
-        'AddLegacyBranchFor37' => [],
-
-        'ResetWebRootDir-2' => [],
-
-        'CheckoutDevMaster-2' => [],
-
-        'AddUpgradeBranch' => [],
-
-        'UpdateComposerRequirements-1' => [
-            'Package' => 'silverstripe/framework',
-            'NewVersion' => '~3.7',
-        ],
-        'UpdateComposerRequirements-2' => [
-            'Package' => 'silverstripe/cms',
-            'NewVersion' => '~3.7',
-        ],
-        'UpdateComposerRequirements-3' => [
-            'Package' => 'silverstripe/reports',
-            'NewVersion' => '~3.7',
-        ],
-        'UpdateComposerRequirements-4' => [
-            'Package' => 'silverstripe/siteconfig',
-            'NewVersion' => '~3.7',
-        ],
-        'ResetWebRootDir-3' => [],
-
-        //Step2: MoveToNewVersion
-        'ComposerInstallProject' => [],
-
-        //Step3: FixBeforeStart
-        'SearchAndReplace' => [
-            'ToFolder' => 'SS34',
-        ],
-
-        'SearchAndReplace' => [
-            'ToFolder' => 'SS36',
-        ],
-
-        'SearchAndReplace' => [
-            'ToFolder' => 'SS37',
-        ],
-
-        //Step4: CoreUpgrade
-        'FinalDevBuild37' => [],
-
-        //step7: Lock-in
-        // 'FinaliseUpgradeWithMergeIntoMaster' => [],
-    ];
-
     protected $listOfTasks = [
         'CheckThatFoldersAreReady' => [],
         'ResetWebRootDir-1' => [],

e660710a2f1e08bc314f3a3bf33cc0f9d9891407
LABCAT <shane.watters.nz@gmail.com>
2019-12-03 11:16:29 +1300

    Improvements to Ss31ToSs37 recipe.

--- src/UpgradeRecipes/Ss31ToSs37.php
+++ src/UpgradeRecipes/Ss31ToSs37.php
@@ -86,5 +86,39 @@ class Ss33ToSs37 extends BaseClass
         // 'FinaliseUpgradeWithMergeIntoMaster' => [],
     ];
 
+    protected $listOfTasks = [
+        'CheckThatFoldersAreReady' => [],
+        'ResetWebRootDir-1' => [],
+
+        'CheckoutDevMaster-1' => [],
+        'AddLegacyBranchFor37' => [
+            'nameOfLegacyBranch' => 'upgrade37'
+        ],
+
+        'ResetWebRootDir-2' => [],
+
+        'CheckoutDevMaster-2' => [],
+
+        'AddUpgradeBranch' => [],
+
+        'UpdateComposerRequirements-1' => [
+            'Package' => 'silverstripe/framework',
+            'NewVersion' => '~3.7',
+        ],
+        'UpdateComposerRequirements-2' => [
+            'Package' => 'silverstripe/cms',
+            'NewVersion' => '~3.7',
+        ],
+        'SearchAndReplace-1' => [
+            'ToFolder' => 'SS34',
+        ],
+        'SearchAndReplace-2' => [
+            'ToFolder' => 'SS36',
+        ],
+        'SearchAndReplace-3' => [
+            'ToFolder' => 'SS37',
+        ],
+    ];
+
     protected $frameworkComposerRestraint = '~3.7@stable';
 }

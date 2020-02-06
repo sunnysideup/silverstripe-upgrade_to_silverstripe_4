fcb7bc59f06b7658ad5f4f7985cefe4a1fd7e7c5
Nicolaas - Sunny Side Up <nt@sunnysideup.co.nz>
2020-02-06 21:37:57 +1300

    PATCH: cleanup

---
+++ src/Interfaces/ModuleUpgraderInterface.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Sunnysideup\UpgradeToSilverstripe4\Interfaces;
+
+interface ModuleUpgraderInterface
+{
+}
---
+++ src/Traits/Creator.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Sunnysideup\UpgradeToSilverstripe4\Traits;
+
+trait Creator
+{
+    /**
+     * Holds the only instance of me
+     * @var mixed
+     */
+    protected static $singleton = null;
+
+    public function destroy()
+    {
+        self::$singleton = null;
+    }
+
+    /**
+     * Create the only instance of me and return it
+     * @return mixed
+     */
+    public static function create()
+    {
+        if (self::$singleton === null) {
+            $className = static::class;
+            self::$singleton = new $className();
+        }
+        return self::$singleton;
+    }
+}

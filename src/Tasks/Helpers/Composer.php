b0deb1a308ed1f5f8c8ee64cdaacc6727db5dfd8
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-05-19 14:13:39 +1200

    PATCH: cleanup composer

--- src/Tasks/Helpers/Composer.php
+++ src/Tasks/Helpers/Composer.php
@@ -4,4 +4,26 @@ namespace Sunnysideup\UpgradeToSilverstripe4\Tasks\Helpers;
 
 class Composer
 {
+    protected static $inst = null;
+
+    protected $myMu = null;
+
+    public static function inst($mu)
+    {
+        if (self::$inst === null) {
+            self::$inst = new Composer();
+            self::$inst->setMu($mu);
+        }
+        return self::$inst;
+    }
+
+    public function DumpAutoload(string $dir)
+    {
+        $this->mu()->execMe(
+            $dir,
+            'composer dumpautoload',
+            'run composer dumpautoload',
+            false
+        );
+    }
 }

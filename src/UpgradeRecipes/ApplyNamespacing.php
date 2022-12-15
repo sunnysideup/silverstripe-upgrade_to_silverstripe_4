e2803078bade990ea4c98938da0279adfe128279
Nicolaas @ Tappy @ Sunny Side Up <github@sunnysideup.co.nz>
2022-12-16 00:24:46 +1300

    WIP

---
+++ src/UpgradeRecipes/ApplyNamespacing.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Sunnysideup\UpgradeToSilverstripe4\UpgradeRecipes;
+
+class ApplyNamespacing extends BaseClass
+{
+    /**
+     * name of the branch to be created that we use a starter branch for upgrade
+     * @var string branch name
+     */
+    protected $nameOfUpgradeStarterBranch = 'upgrades/starting-point/apply-namespacing-start';
+
+    /**
+     * name of the branch created to do the upgrade
+     * @var string branch name
+     */
+    protected $nameOfTempBranch = 'upgrades/automatedbranch/apply-namespacing-do';
+
+    /**
+     * The default namespace for all tasks
+     * @var string
+     */
+    protected $defaultNamespaceForTasks = 'Sunnysideup\UpgradeToSilverstripe4\Tasks\IndividualTasks';
+
+    #########################################
+    # TASKS
+    #########################################
+
+    /**
+     * An array of all the 'taskNames of the tasks that you wish to run during the execution of this upgrader task.
+     * This array can be overriden in the example-index.php file that you create.
+     * You can enter a full name space if you need to.
+     * The final -x will be removed.  We add -1 or -2 to run the same task multiple times.
+     *
+     * @var array
+     */
+    protected $listOfTasks = [
+        //Step1: Prepare
+        //Step4: CoreUpgrade
+        'AddNamespace' => [],
+        'Upgrade' => [],
+    ];
+
+    protected $frameworkComposerRestraint = '~4@stable';
+}
